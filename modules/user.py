d2c06379875882b61980b66cafad1ddb65541cb8
Ragis Rahmatulloh <77946797+icarusnp1@users.noreply.github.com>
2025-05-29 23:53:30 +0700

    regex

--- modules/user.py
+++ modules/user.py
@@ -1,8 +1,19 @@
 # modules/user.py
+import re
 from werkzeug.security import generate_password_hash, check_password_hash
 from modules.db import insert_user, check_user
 
 def register_user(username, email, password):
+    # Validasi regex password
+    pattern = re.compile(
+        r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])(?=.{8,})(?!.*[*\'=]).*$'
+    )
+    if not pattern.match(password):
+        return {
+            'success': False,
+            'message': 'Password minimal 8 karakter, mengandung huruf besar, huruf kecil, angka, simbol, dan tidak boleh mengandung *, \' atau ='
+        }
+
     if check_user(email):
         return {'success': False, 'message': 'Email sudah terdaftar'}
 
